7fb794f4da9cf0c01bb5c42324cfc60177126471
evafan1337 <as007ershov@gmail.com>
2019-04-11 05:42:49 +0500

    Doing first task

--- index.php
+++ index.php
@@ -65,14 +65,14 @@ $show_complete_tasks = rand(0, 1);
                 <div class="tasks-controls">
                     <nav class="tasks-switch">
                         <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-                        <a href="/" class="tasks-switch__item">Повестка дня</a>
+                        <a href="/" class="tasks-switch__item">Повестка дня</a> 
                         <a href="/" class="tasks-switch__item">Завтра</a>
                         <a href="/" class="tasks-switch__item">Просроченные</a>
                     </nav>
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1): ?>checked <?php endif; ?>> 
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -93,6 +93,18 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    
+                    <tr class="tasks__item task task--completed" <?php if ($show_complete_tasks == 0): ?>style="display: none" <?php endif; ?>>
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+                        <td class="task__date">10.10.2019</td>
+                        <td class="task__controls"></td>
+                    </tr>
+
                 </table>
             </main>
         </div>
